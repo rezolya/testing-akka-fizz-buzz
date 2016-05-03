7985aadfdad6852b33fa43fc8509e37bff48bd6b
Olga.Reznik <Olga.Reznik@ing.nl>
2016-05-03 22:55:31 +0200

    added exercise

---
+++ src/test/scala/fizzbuzz/DivisibilitySpec.scala
@@ -0,0 +1,19 @@
+package fizzbuzz
+
+import org.scalatest.{Matchers, WordSpec}
+
+class DivisibilitySpec extends WordSpec with Matchers {
+
+  //TODO: 0.Test the methods that check divisibility in Fizz and Buzz
+  //Note: you may have to refactor the actors
+  "Divisibility trait" should {
+    "determine if number is divisible by 5" in {
+      ???
+    }
+
+    "determine if number is divisible by 3" in {
+      ???
+    }
+  }
+}
+
